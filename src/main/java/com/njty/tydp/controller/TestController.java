901e37073b3ec0a7c1aadc5504d34dff9fd165fb
gesofts <250058349@qq.com>
2018-09-04 15:41:58 +0800

    日志

--- src/main/java/com/njty/tydp/controller/TestController.java
+++ src/main/java/com/njty/tydp/controller/TestController.java
@@ -2,6 +2,8 @@ package com.njty.tydp.controller;
 
 import com.njty.tydp.model.TestModel;
 import com.njty.tydp.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,8 @@ import java.util.Map;
 @RequestMapping("/test")
 public class TestController {
 
+    protected static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @Autowired
     private TestService testService;
 
@@ -24,6 +28,7 @@ public class TestController {
 
     @RequestMapping("/list2")
     public List<Map<String, Object>> findTestInfo() {
+        logger.info("TestController - findTestInfo");
         return  testService.findTestInfo();
     }
 }
