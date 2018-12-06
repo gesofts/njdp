07f2dd3b9b261dc8dd35d97803a1527e0c4fc205
gesofts <250058349@qq.com>
2018-12-06 15:53:26 +0800

    加入基础操作处理

--- src/main/java/com/njty/tydp/commom/EntityController.java
+++ src/main/java/com/njty/tydp/commom/EntityController.java
@@ -17,8 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 /**
